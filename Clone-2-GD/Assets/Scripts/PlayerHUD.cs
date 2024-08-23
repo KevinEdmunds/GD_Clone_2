7d5eadb60b99f0f7425fd8361548b83a6045fd2c
NTTFBS <2551187@students.wits.ac.za>
2024-08-23 06:40:47 -0400

    I dont know

--- Clone-2-GD/Assets/Scripts/PlayerHUD.cs
+++ Clone-2-GD/Assets/Scripts/PlayerHUD.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 using UnityEngine.UI;
-using Org.BouncyCastle.Asn1.BC;
-using Org.BouncyCastle.Bcpg;
 
 public class PlayerHUD : NetworkBehaviour
 {
@@ -13,7 +11,6 @@ public class PlayerHUD : NetworkBehaviour
     public PlayerActions playerActions;
     public Button ventButton, useButton, reportButton, killButton, sabotageButton;
 
-    //public GameObject emptyGarbageCafeTrigger;
     private GameObject emptyGarbageCafePanel;
     private GameObject cardSwipeAdminPanel;
     private GameObject downloadWeaponsPanel;
@@ -23,46 +20,79 @@ public class PlayerHUD : NetworkBehaviour
     private Collider2D cardSwipeAdminTrigger;
     private Collider2D downloadWeaponsTrigger;
 
-    // Initialize HUD elements
-
     public override void OnStartClient()
     {
-        if (isLocalPlayer) 
-        { 
-       
         base.OnStartClient();
 
+        if (isLocalPlayer)
+        {
+            playerCollider = GetComponent<Collider2D>();
 
-        playerCollider = GetComponent<Collider2D>();
+            GameObject emptyGarbageCafetrigger = GameObject.Find("EmptyGarbageCafeTrigger");
+            GameObject cardSwipeAdmintrigger = GameObject.Find("CardSwipeAdminTrigger");
+            GameObject downloadWeaponstrigger = GameObject.Find("DownloadWeaponsTrigger");
 
-        GameObject emptyGarbageCafetrigger = GameObject.Find("EmptyGarbageCafeTrigger");
-        GameObject cardSwipeAdmintrigger = GameObject.Find("CardSwipeAdminTrigger");
-        GameObject downloadWeaponstrigger = GameObject.Find("DownloadWeaponsTrigger");
+            if (emptyGarbageCafetrigger != null)
+            {
+                emptyGarbageCafeTrigger = emptyGarbageCafetrigger.GetComponent<Collider2D>();
+            }
+            else
+            {
+                Debug.LogError("EmptyGarbageCafeTrigger not found!");
+            }
 
-        emptyGarbageCafeTrigger = emptyGarbageCafetrigger.GetComponent<Collider2D>();
-        cardSwipeAdminTrigger = cardSwipeAdmintrigger.GetComponent<Collider2D>();
-        downloadWeaponsTrigger = downloadWeaponstrigger.GetComponent<Collider2D>();
+            if (cardSwipeAdmintrigger != null)
+            {
+                cardSwipeAdminTrigger = cardSwipeAdmintrigger.GetComponent<Collider2D>();
+            }
+            else
+            {
+                Debug.LogError("CardSwipeAdminTrigger not found!");
+            }
 
+            if (downloadWeaponstrigger != null)
+            {
+                downloadWeaponsTrigger = downloadWeaponstrigger.GetComponent<Collider2D>();
+            }
+            else
+            {
+                Debug.LogError("DownloadWeaponsTrigger not found!");
+            }
 
+            emptyGarbageCafePanel = GameObject.Find("EmptyGarbageCafePanel");
+            if (emptyGarbageCafePanel != null)
+            {
+                emptyGarbageCafePanel.SetActive(false);
+            }
+            else
+            {
+                Debug.LogError("EmptyGarbageCafePanel not found!");
+            }
 
-        emptyGarbageCafePanel = GameObject.Find("EmptyGarbageCafePanel");
-        emptyGarbageCafePanel.SetActive(false);
+            cardSwipeAdminPanel = GameObject.Find("CardSwipeTaskPanel");
+            if (cardSwipeAdminPanel != null)
+            {
+                cardSwipeAdminPanel.SetActive(false);
+            }
+            else
+            {
+                Debug.LogError("CardSwipeTaskPanel not found!");
+            }
 
-        cardSwipeAdminPanel = GameObject.Find("CardSwipeTaskPanel");
-        cardSwipeAdminPanel.SetActive(false);
-
-        downloadWeaponsPanel = GameObject.Find("DownloadTaskWeaponsPanel1");
-        downloadWeaponsPanel.SetActive(false);
+            downloadWeaponsPanel = GameObject.Find("DownloadTaskWeaponsPanel1");
+            if (downloadWeaponsPanel != null)
+            {
+                downloadWeaponsPanel.SetActive(false);
+            }
+            else
+            {
+                Debug.LogError("DownloadTaskWeaponsPanel1 not found!");
+            }
         }
-
     }
 
     void Start()
     {
-        
-
-
-
         if (isLocalPlayer)
         {
             GameObject.FindGameObjectWithTag("MainCamera").transform.parent = this.transform;
@@ -72,10 +102,6 @@ public class PlayerHUD : NetworkBehaviour
             reportButton.interactable = false;
             useButton.interactable = false;
         }
-
-        
-       
-
     }
 
     public void KillButton()
@@ -99,22 +125,18 @@ public class PlayerHUD : NetworkBehaviour
         {
             if (!playerType.isImposter)
             {
-                if (playerCollider.IsTouching(emptyGarbageCafeTrigger))
+                if (playerCollider != null && emptyGarbageCafeTrigger != null && playerCollider.IsTouching(emptyGarbageCafeTrigger))
                 {
                     emptyGarbageCafePanel.SetActive(true);
                 }
-                else if (playerCollider.IsTouching(cardSwipeAdminTrigger))
+                else if (playerCollider != null && cardSwipeAdminTrigger != null && playerCollider.IsTouching(cardSwipeAdminTrigger))
                 {
-                    
                     cardSwipeAdminPanel.SetActive(true);
-                    
                 }
-                else if (playerCollider.IsTouching(downloadWeaponsTrigger))
+                else if (playerCollider != null && downloadWeaponsTrigger != null && playerCollider.IsTouching(downloadWeaponsTrigger))
                 {
                     downloadWeaponsPanel.SetActive(true);
                 }
-                    
-                
             }
         }
     }
@@ -124,32 +146,21 @@ public class PlayerHUD : NetworkBehaviour
         //link to command to spawn "task" for player
     }
 
-    public void Update()
+    void Update()
     {
         if (isLocalPlayer)
         {
             if (!playerType.isImposter)
             {
-                if (playerCollider.IsTouching(emptyGarbageCafeTrigger))
-                {
-                    useButton.interactable = true;
-                
-                }
-                else
-                {
-                    useButton.interactable = false;
-                }
-
-                if (playerCollider.IsTouching(cardSwipeAdminTrigger))
+                if (playerCollider != null && emptyGarbageCafeTrigger != null && playerCollider.IsTouching(emptyGarbageCafeTrigger))
                 {
                     useButton.interactable = true;
                 }
-                else
+                else if (playerCollider != null && cardSwipeAdminTrigger != null && playerCollider.IsTouching(cardSwipeAdminTrigger))
                 {
-                    useButton.interactable = false;
+                    useButton.interactable = true;
                 }
-
-                if (playerCollider.IsTouching(downloadWeaponsTrigger))
+                else if (playerCollider != null && downloadWeaponsTrigger != null && playerCollider.IsTouching(downloadWeaponsTrigger))
                 {
                     useButton.interactable = true;
                 }
