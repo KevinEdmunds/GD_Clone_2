944af0b42e4f62fb343502c3c65d29228c8444fa
NTTFBS <2551187@students.wits.ac.za>
2024-08-23 06:17:08 -0400

    Download/Upload Task (Problem)
    Download task has problem in PlayerHUD Script

--- Clone-2-GD/Assets/Scripts/PlayerHUD.cs
+++ Clone-2-GD/Assets/Scripts/PlayerHUD.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 using UnityEngine.UI;
+using Org.BouncyCastle.Asn1.BC;
+using Org.BouncyCastle.Bcpg;
 
 public class PlayerHUD : NetworkBehaviour
 {
@@ -11,10 +13,57 @@ public class PlayerHUD : NetworkBehaviour
     public PlayerActions playerActions;
     public Button ventButton, useButton, reportButton, killButton, sabotageButton;
 
+    //public GameObject emptyGarbageCafeTrigger;
+    private GameObject emptyGarbageCafePanel;
+    private GameObject cardSwipeAdminPanel;
+    private GameObject downloadWeaponsPanel;
+
+    private Collider2D playerCollider;
+    private Collider2D emptyGarbageCafeTrigger;
+    private Collider2D cardSwipeAdminTrigger;
+    private Collider2D downloadWeaponsTrigger;
+
     // Initialize HUD elements
+
+    public override void OnStartClient()
+    {
+        if (isLocalPlayer) 
+        { 
+       
+        base.OnStartClient();
+
+
+        playerCollider = GetComponent<Collider2D>();
+
+        GameObject emptyGarbageCafetrigger = GameObject.Find("EmptyGarbageCafeTrigger");
+        GameObject cardSwipeAdmintrigger = GameObject.Find("CardSwipeAdminTrigger");
+        GameObject downloadWeaponstrigger = GameObject.Find("DownloadWeaponsTrigger");
+
+        emptyGarbageCafeTrigger = emptyGarbageCafetrigger.GetComponent<Collider2D>();
+        cardSwipeAdminTrigger = cardSwipeAdmintrigger.GetComponent<Collider2D>();
+        downloadWeaponsTrigger = downloadWeaponstrigger.GetComponent<Collider2D>();
+
+
+
+        emptyGarbageCafePanel = GameObject.Find("EmptyGarbageCafePanel");
+        emptyGarbageCafePanel.SetActive(false);
+
+        cardSwipeAdminPanel = GameObject.Find("CardSwipeTaskPanel");
+        cardSwipeAdminPanel.SetActive(false);
+
+        downloadWeaponsPanel = GameObject.Find("DownloadTaskWeaponsPanel1");
+        downloadWeaponsPanel.SetActive(false);
+        }
+
+    }
+
     void Start()
     {
-        if(isLocalPlayer)
+        
+
+
+
+        if (isLocalPlayer)
         {
             GameObject.FindGameObjectWithTag("MainCamera").transform.parent = this.transform;
             playerHUD.SetActive(true);
@@ -23,6 +72,10 @@ public class PlayerHUD : NetworkBehaviour
             reportButton.interactable = false;
             useButton.interactable = false;
         }
+
+        
+       
+
     }
 
     public void KillButton()
@@ -42,11 +95,69 @@ public class PlayerHUD : NetworkBehaviour
 
     public void UseButton()
     {
-
+        if (isLocalPlayer)
+        {
+            if (!playerType.isImposter)
+            {
+                if (playerCollider.IsTouching(emptyGarbageCafeTrigger))
+                {
+                    emptyGarbageCafePanel.SetActive(true);
+                }
+                else if (playerCollider.IsTouching(cardSwipeAdminTrigger))
+                {
+                    
+                    cardSwipeAdminPanel.SetActive(true);
+                    
+                }
+                else if (playerCollider.IsTouching(downloadWeaponsTrigger))
+                {
+                    downloadWeaponsPanel.SetActive(true);
+                }
+                    
+                
+            }
+        }
     }
 
     public void Sabotage()
     {
         //link to command to spawn "task" for player
+    }
+
+    public void Update()
+    {
+        if (isLocalPlayer)
+        {
+            if (!playerType.isImposter)
+            {
+                if (playerCollider.IsTouching(emptyGarbageCafeTrigger))
+                {
+                    useButton.interactable = true;
+                
+                }
+                else
+                {
+                    useButton.interactable = false;
+                }
+
+                if (playerCollider.IsTouching(cardSwipeAdminTrigger))
+                {
+                    useButton.interactable = true;
+                }
+                else
+                {
+                    useButton.interactable = false;
+                }
+
+                if (playerCollider.IsTouching(downloadWeaponsTrigger))
+                {
+                    useButton.interactable = true;
+                }
+                else
+                {
+                    useButton.interactable = false;
+                }
+            }
+        }
     }
 }
